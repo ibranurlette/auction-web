184d1bee7c199daab203ef87e1bc6881e3b28b6f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2023-09-21 12:42:12 +0700

    responsive message page

--- src/pages/Message/ListMessage.tsx
+++ src/pages/Message/ListMessage.tsx
@@ -8,15 +8,16 @@ interface ListMessageProps {
 
 export const ListMessage = ({ data }: ListMessageProps) => {
 	return (
-		<div className="basis-1/3 mt-10">
+		<div className="lg:basis-1/3 mt-10 p-4">
 			{data.map((message, index) => (
-				<div key={index} className="flex items-center mb-5 mx-5">
+				<div key={index} className="flex items-center mb-5 ">
 					<img
 						className="h-14 w-14 rounded-full mr-4"
 						src={message.image}
 						alt="nature image"
 					/>
-					<div>
+
+					<div className="max-sm:hidden sm:hidden lg:inline">
 						<div className="font-medium text-sm">{message.name}</div>
 						<div className="font-normal text-xs text-gray-600">
 							{message.status}
